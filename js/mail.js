83272858a075f6fde6361e376413996a31102f00
Alejandro Orbe / SPartan 2502 <josealexorbech@hotmail.com>
2023-11-22 14:25:51 -0600

    email

--- js/mail.js
+++ js/mail.js
@@ -1,25 +1,20 @@
-
-
-
-
 const btn = document.getElementById('contactButton');
+console.log(btn);
 const form = document.getElementById('contactForm')
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    btn.value = 'Sending...';
+    btn.value = 'Enviando...';
 
     const serviceID = 'default_service';
     const templateID = 'template_9sorqkq';
 
     emailjs.sendForm(serviceID, templateID, form)
         .then(() => {
-            btn.value = 'Send Email';
-            alert('Sent!');
+            btn.value = 'Enviar';
+            alert('Enviado! en cuanto lo vea me pondre en contacto.');
         }, (err) => {
-            btn.value = 'Send Email';
+            btn.value = 'Enviar';
             alert(JSON.stringify(err));
         });
 });
-
-
